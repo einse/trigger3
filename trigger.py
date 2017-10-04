822f8bc7383b4f78d9752edfe5a0679d16060610
Serguey Arsentev <einse@yandex.ru>
2017-10-04 15:54:14 +0300

    Folder index file is generated

--- trigger.py
+++ trigger.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import time
+import codecs
 start_path = u'/media'
 tags = [u'-мкрк', u'-диз', u'мкрк-', u'диз-']
 count_of_target_files_in_folder = 0
@@ -16,7 +17,7 @@ for current_folder, folders, files in os.walk(start_path):
                     = count_of_target_files_in_folder + 1
                 if count_of_target_files_in_folder == 1:
                     current_path_of_file\
-                        = current_folder + '/' + current_file
+                        = current_folder + u'/' + current_file
                     date_of_the_first_target_file_in_folder__unix\
                         = time.gmtime(os.stat(current_path_of_file).st_mtime)
                     date_of_the_first_target_file_in_folder__human_cn\
@@ -33,10 +34,21 @@ for current_folder, folders, files in os.walk(start_path):
             = [len(files)\
             , count_of_target_files_in_folder\
             , current_folder]
-for i, id_ in enumerate(folders_collection):
-    print u'{}\t{}\t{}'\
-        .format\
-        ( folders_collection[id_][0]\
-        , folders_collection[id_][1]\
-        , id_\
-        )
+current_time__unix = time.localtime()
+current_time__human_cn\
+    = time.strftime(u'%Y-%m-%d_%H-%M-%S', current_time__unix)
+index_root = u'trigger-index'
+folders_index_path\
+    = u'./' + index_root + u'/'\
+    + current_time__human_cn + u'_folders-index.txt'
+with codecs.open(folders_index_path, 'a', encoding='utf-8') as f:
+    for i, id_ in enumerate(folders_collection):
+        folders_index_string = u'{}\t{}\t{}\t{}'\
+            .format\
+            ( folders_collection[id_][0]\
+            , folders_collection[id_][1]\
+            , id_\
+            , folders_collection[id_][2]\
+            )
+        f.write(folders_index_string)
+        f.write(u'\n')
